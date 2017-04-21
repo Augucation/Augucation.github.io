9018c41e550a46c62b29312548516a7e289d026f
juuuuul <juliaa_hertel@web.de>
2017-04-21 17:17:08 +0200

    2_48: added Klangfarbe

--- augucations/2/48/Augucation2_48.js
+++ augucations/2/48/Augucation2_48.js
@@ -1,16 +1,23 @@
 var 	span_vol,
 		span_freq,
+		min_f_log = Math.log(20),
+		max_f_log = Math.log(20000),
+		old_v, 
+		old_f,
 		sound = new Pizzicato.Sound();
+
+var radios = document.getElementsByName("type");
 
 function init(){
 	findElements();
+	manageRadioButtons();
 	startSound();
 }
 
 function startSound(){
 	sound = new Pizzicato.Sound({ 
 			source: 'wave',
-			options: { type: 'sine', frequency: frequency, volume: v }
+			options: { type: 'sine', frequency: 400, volume: 0.5 }
 	});
 	sound.play();	
 }
@@ -22,12 +29,42 @@ function findElements(){
 
 function setVolume(val){
 	sound.volume = parseFloat(val);	
-	span_vol.innerHTML = v;
+	span_vol.innerHTML = Math.round(val * 100) + "%";
 }
 
 function setFrequency(val){
-	sound.frequency = parseFloat(val);
-	span_freq.innerHTML = frequency;
+	sound.frequency = logFreq(val);
+	span_freq.innerHTML = logFreq(val) + " Hz";
+}
+
+function logFreq(val){
+	var scale = (max_f_log - min_f_log) / 100;
+	return Math.round( Math.exp(min_f_log + scale * val));
+	
+}
+
+function setType(val){
+	
+	// save volume and frequency of the old sound
+	old_v = sound.volume;
+	old_f = sound.frequency;
+	
+	sound.stop();
+	sound = new Pizzicato.Sound({ 
+			source: 'wave',
+			options: { type: val, frequency: old_f, volume: old_v }
+	});
+	sound.play();		
+}
+
+function manageRadioButtons()
+{	
+	for(var i = 0; i < radios.length; i++){
+		radios[i].onclick = function()
+		{
+			setType(this.id);
+		}
+	}
 }
 
 init();
