ffc905175e66ca21d1413a0e21ed432b77ae7ca0
juuuuul <juliaa_hertel@web.de>
2017-04-26 00:30:15 +0200

    hihihihihi-1

--- resources/js/plugins/pdf.js/pdf.js
+++ resources/js/plugins/pdf.js/pdf.js
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//////////////dskjhdsajhdkjsahdjsahdjsahdksahdjkha!!!!!!!!!!!!!!
 
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
