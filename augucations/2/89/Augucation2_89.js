adec645968cf8ce36841870997709ef8bfa29b10
juuuuul <juliaa_hertel@web.de>
2017-05-02 12:07:58 +0200

    2_89: removed visualization

--- augucations/2/89/Augucation2_89.js
+++ augucations/2/89/Augucation2_89.js
@@ -10,12 +10,6 @@ var 	span_vol_1,
 		muteBtn = document.getElementById("muteBtn"),
 		img_soundoff = "url('https://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg')",
 		img_soundon = "url('https://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg')",
-		c = document.getElementById("myCanvas"),
-		ctx = c.getContext("2d"),
-		c_x_min = 25,
-		c_x_max = 940,
-		c_y_min = 20,
-		c_y_max = 132,
 		freq_slider1 = document.getElementById("slider_freq1"),
 		freq_slider2 = document.getElementById("slider_freq2"),
 		vol_slider1 = document.getElementById("slider_vol1"),
@@ -25,7 +19,6 @@ var 	span_vol_1,
 function init(){
 	findElements();
 	createSound();
-	draw();
 }
 
 function createSound(){
@@ -58,8 +51,6 @@ function setVolume(id, val){
 		sound2.volume = parseFloat(val);
 		span_vol_2.innerHTML =  Math.round(val * 100) + "%";
 	}
-
-	draw();
 }
 
 function setFrequency(id, val){
@@ -72,8 +63,6 @@ function setFrequency(id, val){
 		sound2.frequency = parseFloat(val);
 		span_freq_2.innerHTML = val + " Hz";
 	}
-
-	draw();
 }
 
 function logFreq(val){
@@ -98,54 +87,6 @@ function mute(){
 		play = true;
 		muteBtn.style.backgroundImage = img_soundon;
 	}
-}
-
-function calcFreqDrawX(num){
-	if(num == 1)
-		return c_x_min + (freq_slider1.value - 200) * 0.001 * (c_x_max - c_x_min);
-	else if(num == 2)
-		return c_x_min + (freq_slider2.value - 200) * 0.001 * (c_x_max - c_x_min);
-}
-
-function calcVolDrawY(num){
-	if(num == 1)
-		return c_y_min + (1 - vol_slider1.value) * (c_y_max - c_y_min);
-	else if(num == 2)
-		return c_y_min + (1 - vol_slider2.value) * (c_y_max - c_y_min);
-}
-
-function calcControlPoint(){
-	return c_y_min + (1 - vol_slider1.value * 2) * (c_y_max - c_y_min);
-}
-
-function draw(){
-	var maskOffsetX = 50;
-
-	ctx.clearRect(0, 0, c.width, c.height);
-
-	// sound 2
-	ctx.beginPath();
-	ctx.moveTo(calcFreqDrawX(2), calcVolDrawY(2));
-	ctx.lineTo(calcFreqDrawX(2), c_y_max);
-	ctx.strokeStyle = "#FF0000";
-	ctx.lineWidth = 5;
-	ctx.stroke();
-
-	// sound 1
-	ctx.beginPath();
-	ctx.moveTo(calcFreqDrawX(1), calcVolDrawY(1));
-	ctx.lineTo(calcFreqDrawX(1), c_y_max);
-	ctx.strokeStyle = "#0000FF";
-	ctx.lineWidth = 5;
-	ctx.stroke();
-
-	// mask
-	ctx.beginPath();
-	ctx.moveTo(calcFreqDrawX(1) - maskOffsetX, c_y_max);
-	ctx.quadraticCurveTo(calcFreqDrawX(1), calcControlPoint(), calcFreqDrawX(1) + maskOffsetX, c_y_max);
-	ctx.strokeStyle = "#555555";
-	ctx.lineWidth = 3;
-	ctx.stroke();
 }
 
 init();
