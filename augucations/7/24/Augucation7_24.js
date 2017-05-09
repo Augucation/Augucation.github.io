37763d5eb04cf204c9b504cd626cc9aa6ce837de
juuuuul <juliaa_hertel@web.de>
2017-05-09 08:02:30 +0200

    Phasenverschiebung - not finished

--- augucations/7/24/Augucation7_24.js
+++ augucations/7/24/Augucation7_24.js
@@ -1,9 +1,10 @@
-var 	delay, 
+var 	delay,
 		play = false,
 		muteBtn = document.getElementById("muteBtn"),
 		img_soundoff = "url('https://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg')",
 		img_soundon = "url('https://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg')",
-		span_val,
+		span_high,
+		span_low,
 		sound1,
 		sound2;
 
@@ -16,31 +17,49 @@ function init(){
 }
 
 function createSounds(){
-	sound1 = new Audio('../../../resources/audio/Chantar_short.ogg');
-	sound2 = new Audio('../../../resources/audio/Chantar_short.ogg');
+	//sound1 = new Audio('../../../resources/audio/Chantar_short.ogg');
+	//sound2 = new Audio('../../../resources/audio/Chantar_short.ogg');
+
+    sound1 = new Pizzicato.Sound({
+			source: 'wave',
+			options: { type: 'sine', frequency: 400, volume: 0.5 }
+	});
+
+    var flanger = new Pizzicato.Effects.Flanger({
+        time: 0.45,
+        speed: 0.2,
+        depth: 0.1,
+        feedback: 0.1,
+        mix: 0.5
+    });
+
+    sound1.addEffect(flanger);
 }
 
 function findElements(){
-	span_val = document.getElementById("span_val");
+	span_high = document.getElementById("span_high");
+	span_low = document.getElementById("span_low");
 }
 
 function setDelay(val){
-	
+
 	span_val.innerHTML =  val + "ms";
-	
+
 	val = parseFloat(val) * 0.001; // s to ms
 	sound2.currentTime = sound1.currentTime + val;
 	console.log("val: ", val, " t1: ", sound1.currentTime, " t2: ", sound2.currentTime);
-	
+
 }
 
 function mute(){
 
 	if(play)
 	{
-		sound1.pause();
-		sound2.pause();
-		play = false;
+		//sound1.pause();
+		//sound2.pause();
+		sound1.stop();
+        sound2.stop();
+        play = false;
 		muteBtn.style.backgroundImage = img_soundoff;
 	}
 	else
@@ -54,13 +73,13 @@ function mute(){
 
 
 function loop(s){
-	
+
 	// if loop is supported, use it
 	if (typeof s.loop == 'boolean')
 	{
 		s.loop = true;
 	}
-	
+
 	// otherwise create own loop function by using an event listener
 	else
 	{
@@ -71,3 +90,5 @@ function loop(s){
 	}
 }
 init();
+
+// sin(x)+sin(x + sin(0.05x)) from 0 to 100
