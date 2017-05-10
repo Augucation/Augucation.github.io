9e92ffca198e3fc83423b73925cba278aeba917c
juuuuul <juliaa_hertel@web.de>
2017-05-10 19:56:11 +0200

    2_180: niceeee visualization

--- augucations/2/180/Augucation2_180.js
+++ augucations/2/180/Augucation2_180.js
@@ -13,7 +13,13 @@ var 	span_vol_1,
 		freq_slider1 = document.getElementById("slider_freq1"),
 		freq_slider2 = document.getElementById("slider_freq2"),
 		vol_slider1 = document.getElementById("slider_vol1"),
-		vol_slider2 = document.getElementById("slider_vol2");
+		vol_slider2 = document.getElementById("slider_vol2"),
+		data1,
+		data2,
+		plot_x_scale = 0.001,
+		plot_x_min = 0,
+		plot_x_max = 100.002,
+		plotFunc;
 
 function init(){
 	findElements();
@@ -29,21 +35,7 @@ function createSound(){
 			source: 'wave',
 			options: { type: 'sine', frequency: 350, volume: 0.5 }
 	});
-
-/*
-	sound1 = new Pizzicato.Sound({
-    source: 'script',
-    options: {
-        audioFunction: function(e) {
-            var output = e.outputBuffer.getChannelData(0);
-            for (var i = 0; i < e.outputBuffer.length; i++)
-								output[i] = sine(i);
-                //output[i] = Math.random();
-        }
-    }
-});
-*/
-
+	
 	sound2 = new Pizzicato.Sound({
 			source: 'wave',
 			options: { type: 'sine', frequency: 350, volume: 0.5 }
@@ -58,18 +50,6 @@ function findElements(){
 	span_freq_2 = document.getElementById("span_freq_2");
 }
 
-function setVolume(id, val){
-
-	if(id == 1){
-		sound1.volume = parseFloat(val);
-		span_vol_1.innerHTML =  Math.round(val * 100) + "%";
-	}
-	else{
-		sound2.volume = parseFloat(val);
-		span_vol_2.innerHTML =  Math.round(val * 100) + "%";
-	}
-}
-
 function setFrequency(id, val){
 
 	if(id == 1){
@@ -80,6 +60,8 @@ function setFrequency(id, val){
 		sound2.frequency = parseFloat(val);
 		span_freq_2.innerHTML = val + " Hz";
 	}
+
+	plotFunc();
 }
 
 function logFreq(val){
@@ -106,4 +88,93 @@ function mute(){
 	}
 }
 
+
+function sampleFunction(x1, x2, func) {
+	var d = [ ];
+	var step = (x2-x1)/3000;
+	for (var i = x1; i < x2; i += step )
+		d.push([i, func( i ) ]);
+
+	return d;
+}
+
+$(function(){
+
+	function sin(x, id){
+		if(id == 1){
+			return 0.5 * Math.sin(sound1.frequency * (2 * Math.PI) * x);
+		}
+		else{
+			return 0.5 * Math.sin(sound2.frequency * (2 * Math.PI) * x);
+		}
+	}
+	
+	function result(x){
+		return sin(x, false) + sin(x, true);
+	}
+
+	function plotit(){
+
+		data1 = sampleFunction( plot_x_min, plot_x_max * plot_x_scale, function(x){ return sin(x, 1); });
+		data2 = sampleFunction( plot_x_min, plot_x_max * plot_x_scale, function(x){ return sin(x, 2); });
+		
+		data3 = sampleFunction( plot_x_min, plot_x_max * plot_x_scale, function(x){ return result(x); });
+		
+		var options_sines =
+		{
+			axisLabels:
+			{
+				show: true
+			},
+			xaxis:
+			{
+				tickFormatter: function(val, axis) {return (val / plot_x_scale).toString() + 'ms';},
+				tickSize: 0.02,
+				axisLabel: 'ms'
+			},
+			yaxis:
+			{
+				min: -1,
+				max: 1,
+				ticks: []
+			},
+			grid:
+			{
+				borderWidth: 0
+			},
+			colors: ["#FF0000", "#0000FF"]
+		};
+
+		var options_result =
+		{
+			axisLabels:
+			{
+				show: true
+			},
+			xaxis:
+			{
+				tickFormatter: function(val, axis) {return (val / plot_x_scale).toString() + 'ms';},
+				tickSize: 0.02,
+				axisLabel: 'ms'
+			},
+			yaxis:
+			{
+				min: -1,
+				max: 1,
+				ticks: []
+			},
+			grid:
+			{
+				borderWidth: 0
+			},
+			colors: ["#FF00FF"]
+		};
+		
+		$.plot($("#chart_sines"), [{data: data1}, {data: data2}], options_sines);
+		$.plot($("#chart_result"), [{data: data3}], options_result);
+	}
+
+	plotit();
+	plotFunc = plotit;
+});
 init();
