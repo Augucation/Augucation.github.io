437912361aa9f8333ad86a7c1072aed2f96a39d0
juuuuul <juliaa_hertel@web.de>
2017-07-14 01:08:21 +0200

    10_30: added some tasks

--- augucations/10/30/task.js
+++ augucations/10/30/task.js
@@ -14,22 +14,41 @@ var tasks = new Array();
 
 function createTasks () {
     new task (
-        "Füge das Bild \"kaese.jpg\" ein.",
-        "Bild einfügen:\n<img src=\"path\"/>",
+        "Füge das Bild \"kaese.jpg\" mit einer Breite von 640 und einer Höhe von 426 ein.",
+        "Bildelement:\n" +
+        "<img />\n\n" +
+        "Quelle:\n" +
+        "src=\"..\"\n\n" +
+        "Breite:\n" +
+        "width=\"..\"\n\n" +
+        "Höhe:\n" +
+        "height=\"..\"\n\n",
         "",
         ""
     );
 
     new task(
-        "Erstelle folgendene Elemente: Hauptüberschrift, Unterüberschrift, Paragraph, Unterüberschrift, Paragraph.",
+        "Gib den Elementen die entsprechenden Elementtypen.",
         "Erste Überschrift:\n<h1>...</h1>\n\nZweite Überschrift:\n<h2>...</h2>\n\nParagraph:\n<p>...</p>",
-        "",
+        "<p>Große Hauptüberschrift!</p>\n" +
+        "<p>Kleinere Unterüberschrift</p>\n" +
+        "<p>Paragraph.</p>\n" +
+        "<p>Kleinere Unterüberschrift</p>\n" +
+        "<p>Paragraph.</p>\n",
         ""
     );
 
     new task(
         "Ergänze die gegebene Tabelle um die Spalte \"Zustand\" und deklariere die erste Zeile als Kopfzeile (table header).",
-        "",
+        "Tabelle:\n" +
+        "<table>\n\n" +
+        "Zeile (table row):\n" +
+        "<tr>\n\n" +
+        "Zelle (table data):\n" +
+        "<td>\n\n" +
+        "Kopfzeilenelement (table header):\n" +
+        "<th>\n",
+
         "<table>\n" +
         "  <tr>\n" +
         "    <td>Produkt</td>\n" +
@@ -47,13 +66,75 @@ function createTasks () {
         ""
     );
 
-    new task("", "", "", "");
-    new task("", "", "", "");
-    new task("", "", "", "");
-    new task("", "", "", "");
-    new task("", "", "", "");
-    new task("", "", "", "");
-    new task("", "", "", "");
+    new task(
+        "Verlinke die Website \"https://www.google.de\" durch einen Hyperlink mit dem Inhalt \"Google\".",
+        "Verweis:\n" +
+        "<a>\n\n" +
+        "Hyperlinkreferenz:\n" +
+        "href=\"...\"",
+        ""
+    );
+
+    new task(
+        "Erstelle einen Button mit der Aufschrift \"Click\", der die Funktion \"f()\" aufruft.",
+        "Eingabeelement:\n" +
+        "<input/>\n\n" +
+        "Typ:\n" +
+        "type=\"...\"\n\n" +
+        "Aufschrift:\n" +
+        "value=\"...\"\n\n" +
+        "Bei Click auszuführender Javascript Code:\n" +
+        "onclick=\"...\"",
+        "",
+        "function f()\n" +
+        "{\n" +
+        "  alert(\"Mööööp.\");\n" +
+        "}"
+    );
+
+    new task(
+        "Erstelle ein 500 x 500 großes SVG-Element mit einem pink gefüllten und grün umrandeten Quadrat mit einer Kantenlänge von 50 und einem rot gefüllten Kreis mit einem Radius von 40 mit dem Mittelpunkt an der Position (100, 50).",
+        "SVG-Element:\n" +
+        "<svg/>\n\n" +
+        "Rechteck-Element:\n" +
+        "<rect/>\n\n" +
+        "Kreis-Element:\n" +
+        "<circle/>\n\n" +
+        "Breite:\n" +
+        "width=\"...\"\n\n" +
+        "Höhe:\n" +
+        "height=\"...\"\n\n" +
+        "Füllfarbe:\n" +
+        "fill=\"...\"\n\n" +
+        "Umrandungsfarbe:\n" +
+        "stroke=\"...\"\n\n" +
+        "Radius:\n" +
+        "r=\"...\"\n\n" +
+        "x-Koordinate des Mittelpunkts:\n" +
+        "cx=\"...\"\n\n" +
+        "y-Koordinate des Mittelpunkts:\n" +
+        "cy=\"...\"\n\n",
+        "",
+        ""
+    );
+
+    new task(
+        "Vervollständige die Funtion increment(), die den Inhalt des Elements mit der Id \"counter\" liest, um 1 inkrementiert und auf den neuen Wert setzt.",
+
+        "Inhalt eines Elements:\n" +
+        "element.innerHTML\n\n" +
+        "Zum Integer parsen:\n" +
+        "parseInt()",
+
+        "<p id=\"counter\">0</p>\n" +
+        "<input type=\"button\" value=\"+ 1\" onclick=\"increment();\"/>",
+
+        "var counter = document.getElementById(\"counter\");\n\n" +
+        "function increment()\n" +
+        "{\n" +
+        "  //TODO: Inhalt von counter lesen, um 1 inkrementieren, Inhalt von counter auf neuen Wert setzen\n" +
+        "}",
+    );
 }
 
 function setTask(idx)
